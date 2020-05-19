d4d49b4b532b69807bc854d31ef16b437180e9ca
Anthony jeamme <anthony@jeamme.fr>
2020-05-19 16:18:53 +0200

    Updates

---
+++ precache-manifest.5b569c5f9bdf58f8d01139082ce67ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700f50c96051659fc00dbdc705432674",
+    "url": "/react-super-useform/index.html"
+  },
+  {
+    "revision": "8aa8e2d188b7c0a541ac",
+    "url": "/react-super-useform/static/css/main.8a1a9008.chunk.css"
+  },
+  {
+    "revision": "08c8d7face5da294db19",
+    "url": "/react-super-useform/static/js/2.0552c23f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-super-useform/static/js/2.0552c23f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa8e2d188b7c0a541ac",
+    "url": "/react-super-useform/static/js/main.84aa52f4.chunk.js"
+  },
+  {
+    "revision": "ba2acec9a299d23c23e7",
+    "url": "/react-super-useform/static/js/runtime-main.6078f799.js"
+  }
+]);
